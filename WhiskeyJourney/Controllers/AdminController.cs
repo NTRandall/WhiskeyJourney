73eb71506b52d45ad30c2f449b6c0e627d39042e
NTRandall <NathanTRandall@gmail.com>
2017-05-06 19:29:58 -0400

    added role drop down list for admins

--- WhiskeyJourney/Controllers/AdminController.cs
+++ WhiskeyJourney/Controllers/AdminController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using PagedList;
 using WhiskeyJourney.Models;
@@ -90,6 +93,87 @@ namespace WhiskeyJourney.Controllers
             }
         }
         #endregion
+
+        // Users *****************************
+
+        // GET: /Admin/Edit/Create 
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult Create()
+        public ActionResult Create()
+        {
+            ExpandedUserDTO objExpandedUserDTO = new ExpandedUserDTO();
+
+            ViewBag.Roles = GetAllRolesAsSelectList();
+
+            return View(objExpandedUserDTO);
+        }
+        #endregion
+
+        // PUT: /Admin/Create
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        #region public ActionResult Create(ExpandedUserDTO paramExpandedUserDTO)
+        public ActionResult Create(ExpandedUserDTO paramExpandedUserDTO)
+        {
+            try
+            {
+                if (paramExpandedUserDTO == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                var Email = paramExpandedUserDTO.Email.Trim();
+                var UserName = paramExpandedUserDTO.Email.Trim();
+                var Password = paramExpandedUserDTO.Password.Trim();
+
+                if (Email == "")
+                {
+                    throw new Exception("No Email");
+                }
+
+                if (Password == "")
+                {
+                    throw new Exception("No Password");
+                }
+
+                // UserName is LowerCase of the Email
+                UserName = Email.ToLower();
+
+                // Create user
+
+                var objNewAdminUser = new ApplicationUser { UserName = UserName, Email = Email };
+                var AdminUserCreateResult = UserManager.Create(objNewAdminUser, Password);
+
+                if (AdminUserCreateResult.Succeeded == true)
+                {
+                    string strNewRole = Convert.ToString(Request.Form["Roles"]);
+
+                    if (strNewRole != "0")
+                    {
+                        // Put user in role
+                        UserManager.AddToRole(objNewAdminUser.Id, strNewRole);
+                    }
+
+                    return Redirect("~/Admin");
+                }
+                else
+                {
+                    ViewBag.Roles = GetAllRolesAsSelectList();
+                    ModelState.AddModelError(string.Empty,
+                        "Error: Failed to create the user. Check password requirements.");
+                    return View(paramExpandedUserDTO);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Roles = GetAllRolesAsSelectList();
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View("Create");
+            }
+        }
+        #endregion
+
         // Utility
 
         #region public ApplicationUserManager UserManager
@@ -120,6 +204,191 @@ namespace WhiskeyJourney.Controllers
             private set
             {
                 _roleManager = value;
+            }
+        }
+        #region private List<SelectListItem> GetAllRolesAsSelectList()
+        private List<SelectListItem> GetAllRolesAsSelectList()
+        {
+            List<SelectListItem> SelectRoleListItems =
+                new List<SelectListItem>();
+
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            var colRoleSelectList = roleManager.Roles.OrderBy(x => x.Name).ToList();
+
+            SelectRoleListItems.Add(
+                new SelectListItem
+                {
+                    Text = "Select",
+                    Value = "0"
+                });
+
+            foreach (var item in colRoleSelectList)
+            {
+                SelectRoleListItems.Add(
+                    new SelectListItem
+                    {
+                        Text = item.Name.ToString(),
+                        Value = item.Name.ToString()
+                    });
+            }
+
+            return SelectRoleListItems;
+        }
+        #endregion
+
+        #endregion
+        // Roles *****************************
+
+        // GET: /Admin/ViewAllRoles
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult ViewAllRoles()
+        public ActionResult ViewAllRoles()
+        {
+            var roleManager =
+                new RoleManager<IdentityRole>
+                (
+                    new RoleStore<IdentityRole>(new ApplicationDbContext())
+                );
+
+            List<RoleDTO> colRoleDTO = (from objRole in roleManager.Roles
+                select new RoleDTO
+                {
+                    Id = objRole.Id,
+                    RoleName = objRole.Name
+                }).ToList();
+
+            return View(colRoleDTO);
+        }
+        #endregion
+
+        // GET: /Admin/AddRole
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult AddRole()
+        public ActionResult AddRole()
+        {
+            RoleDTO objRoleDTO = new RoleDTO();
+
+            return View(objRoleDTO);
+        }
+        #endregion
+
+        // PUT: /Admin/AddRole
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        #region public ActionResult AddRole(RoleDTO paramRoleDTO)
+        public ActionResult AddRole(RoleDTO paramRoleDTO)
+        {
+            try
+            {
+                if (paramRoleDTO == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                var RoleName = paramRoleDTO.RoleName.Trim();
+
+                if (RoleName == "")
+                {
+                    throw new Exception("No RoleName");
+                }
+
+                // Create Role
+                var roleManager =
+                    new RoleManager<IdentityRole>(
+                        new RoleStore<IdentityRole>(new ApplicationDbContext())
+                    );
+
+                if (!roleManager.RoleExists(RoleName))
+                {
+                    roleManager.Create(new IdentityRole(RoleName));
+                }
+
+                return Redirect("~/Admin/ViewAllRoles");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View("AddRole");
+            }
+        }
+        #endregion
+
+        // DELETE: /Admin/DeleteUserRole?RoleName=TestRole
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult DeleteUserRole(string RoleName)
+        public ActionResult DeleteUserRole(string RoleName)
+        {
+            try
+            {
+                if (RoleName == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                if (RoleName.ToLower() == "administrator")
+                {
+                    throw new Exception(String.Format("Cannot delete {0} Role.", RoleName));
+                }
+
+                var roleManager =
+                    new RoleManager<IdentityRole>(
+                        new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+                var UsersInRole = roleManager.FindByName(RoleName).Users.Count();
+                if (UsersInRole > 0)
+                {
+                    throw new Exception(
+                        String.Format(
+                            "Canot delete {0} Role because it still has users.",
+                            RoleName)
+                    );
+                }
+
+                var objRoleToDelete = (from objRole in roleManager.Roles
+                    where objRole.Name == RoleName
+                    select objRole).FirstOrDefault();
+                if (objRoleToDelete != null)
+                {
+                    roleManager.Delete(objRoleToDelete);
+                }
+                else
+                {
+                    throw new Exception(
+                        String.Format(
+                            "Canot delete {0} Role does not exist.",
+                            RoleName)
+                    );
+                }
+
+                List<RoleDTO> colRoleDTO = (from objRole in roleManager.Roles
+                    select new RoleDTO
+                    {
+                        Id = objRole.Id,
+                        RoleName = objRole.Name
+                    }).ToList();
+
+                return View("ViewAllRoles", colRoleDTO);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+
+                var roleManager =
+                    new RoleManager<IdentityRole>(
+                        new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+                List<RoleDTO> colRoleDTO = (from objRole in roleManager.Roles
+                    select new RoleDTO
+                    {
+                        Id = objRole.Id,
+                        RoleName = objRole.Name
+                    }).ToList();
+
+                return View("ViewAllRoles", colRoleDTO);
             }
         }
         #endregion
