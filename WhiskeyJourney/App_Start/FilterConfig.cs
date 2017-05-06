5092fc28a148c87f7e922132d0cd11455cdc3d3c
NTRandall <NathanTRandall@gmail.com>
2017-05-06 18:47:31 -0400

    Add project files.

---
+++ WhiskeyJourney/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WhiskeyJourney
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WhiskeyJourney/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhiskeyJourney.Startup))]
+namespace WhiskeyJourney
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
