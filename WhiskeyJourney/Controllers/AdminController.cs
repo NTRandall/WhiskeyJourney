8fa6aa06da77a1d69600f3e156cf6989655ddcda
NTRandall <NathanTRandall@gmail.com>
2017-05-06 19:42:02 -0400

    added admin views for edit user and roles

--- WhiskeyJourney/Controllers/AdminController.cs
+++ WhiskeyJourney/Controllers/AdminController.cs
@@ -174,72 +174,217 @@ namespace WhiskeyJourney.Controllers
         }
         #endregion
 
-        // Utility
-
-        #region public ApplicationUserManager UserManager
-        public ApplicationUserManager UserManager
+        // GET: /Admin/Edit/TestUser 
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult EditUser(string UserName)
+        public ActionResult EditUser(string UserName)
         {
-            get
+            if (UserName == null)
             {
-                return _userManager ??
-                       HttpContext.GetOwinContext()
-                           .GetUserManager<ApplicationUserManager>();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            private set
+            ExpandedUserDTO objExpandedUserDTO = GetUser(UserName);
+            if (objExpandedUserDTO == null)
             {
-                _userManager = value;
+                return HttpNotFound();
             }
+            return View(objExpandedUserDTO);
         }
         #endregion
 
-        #region public ApplicationRoleManager RoleManager
-        public ApplicationRoleManager RoleManager
+        // PUT: /Admin/EditUser
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        #region public ActionResult EditUser(ExpandedUserDTO paramExpandedUserDTO)
+        public ActionResult EditUser(ExpandedUserDTO paramExpandedUserDTO)
         {
-            get
+            try
             {
-                return _roleManager ??
-                       HttpContext.GetOwinContext()
-                           .GetUserManager<ApplicationRoleManager>();
-            }
-            private set
-            {
-                _roleManager = value;
-            }
-        }
-        #region private List<SelectListItem> GetAllRolesAsSelectList()
-        private List<SelectListItem> GetAllRolesAsSelectList()
-        {
-            List<SelectListItem> SelectRoleListItems =
-                new List<SelectListItem>();
-
-            var roleManager =
-                new RoleManager<IdentityRole>(
-                    new RoleStore<IdentityRole>(new ApplicationDbContext()));
-
-            var colRoleSelectList = roleManager.Roles.OrderBy(x => x.Name).ToList();
-
-            SelectRoleListItems.Add(
-                new SelectListItem
+                if (paramExpandedUserDTO == null)
                 {
-                    Text = "Select",
-                    Value = "0"
-                });
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
 
-            foreach (var item in colRoleSelectList)
-            {
-                SelectRoleListItems.Add(
-                    new SelectListItem
-                    {
-                        Text = item.Name.ToString(),
-                        Value = item.Name.ToString()
-                    });
+                ExpandedUserDTO objExpandedUserDTO = UpdateDTOUser(paramExpandedUserDTO);
+
+                if (objExpandedUserDTO == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return Redirect("~/Admin");
             }
-
-            return SelectRoleListItems;
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View("EditUser", GetUser(paramExpandedUserDTO.UserName));
+            }
         }
         #endregion
 
+        // DELETE: /Admin/DeleteUser
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult DeleteUser(string UserName)
+        public ActionResult DeleteUser(string UserName)
+        {
+            try
+            {
+                if (UserName == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                if (UserName.ToLower() == this.User.Identity.Name.ToLower())
+                {
+                    ModelState.AddModelError(
+                        string.Empty, "Error: Cannot delete the current user");
+
+                    return View("EditUser");
+                }
+
+                ExpandedUserDTO objExpandedUserDTO = GetUser(UserName);
+
+                if (objExpandedUserDTO == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    DeleteUser(objExpandedUserDTO);
+                }
+
+                return Redirect("~/Admin");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View("EditUser", GetUser(UserName));
+            }
+        }
         #endregion
+
+        // GET: /Admin/EditRoles/TestUser 
+        [Authorize(Roles = "Administrator")]
+        #region ActionResult EditRoles(string UserName)
+        public ActionResult EditRoles(string UserName)
+        {
+            if (UserName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UserName = UserName.ToLower();
+
+            // Check that we have an actual user
+            ExpandedUserDTO objExpandedUserDTO = GetUser(UserName);
+
+            if (objExpandedUserDTO == null)
+            {
+                return HttpNotFound();
+            }
+
+            UserAndRolesDTO objUserAndRolesDTO =
+                GetUserAndRoles(UserName);
+
+            return View(objUserAndRolesDTO);
+        }
+        #endregion
+
+        // PUT: /Admin/EditRoles/TestUser 
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        #region public ActionResult EditRoles(UserAndRolesDTO paramUserAndRolesDTO)
+        public ActionResult EditRoles(UserAndRolesDTO paramUserAndRolesDTO)
+        {
+            try
+            {
+                if (paramUserAndRolesDTO == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                string UserName = paramUserAndRolesDTO.UserName;
+                string strNewRole = Convert.ToString(Request.Form["AddRole"]);
+
+                if (strNewRole != "No Roles Found")
+                {
+                    // Go get the User
+                    ApplicationUser user = UserManager.FindByName(UserName);
+
+                    // Put user in role
+                    UserManager.AddToRole(user.Id, strNewRole);
+                }
+
+                ViewBag.AddRole = new SelectList(RolesUserIsNotIn(UserName));
+
+                UserAndRolesDTO objUserAndRolesDTO =
+                    GetUserAndRoles(UserName);
+
+                return View(objUserAndRolesDTO);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View("EditRoles");
+            }
+        }
+        #endregion
+
+        // DELETE: /Admin/DeleteRole?UserName="TestUser&RoleName=Administrator
+        [Authorize(Roles = "Administrator")]
+        #region public ActionResult DeleteRole(string UserName, string RoleName)
+        public ActionResult DeleteRole(string UserName, string RoleName)
+        {
+            try
+            {
+                if ((UserName == null) || (RoleName == null))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                UserName = UserName.ToLower();
+
+                // Check that we have an actual user
+                ExpandedUserDTO objExpandedUserDTO = GetUser(UserName);
+
+                if (objExpandedUserDTO == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (UserName.ToLower() ==
+                    this.User.Identity.Name.ToLower() && RoleName == "Administrator")
+                {
+                    ModelState.AddModelError(string.Empty,
+                        "Error: Cannot delete Administrator Role for the current user");
+                }
+
+                // Go get the User
+                ApplicationUser user = UserManager.FindByName(UserName);
+                // Remove User from role
+                UserManager.RemoveFromRoles(user.Id, RoleName);
+                UserManager.Update(user);
+
+                ViewBag.AddRole = new SelectList(RolesUserIsNotIn(UserName));
+
+                return RedirectToAction("EditRoles", new { UserName = UserName });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+
+                ViewBag.AddRole = new SelectList(RolesUserIsNotIn(UserName));
+
+                UserAndRolesDTO objUserAndRolesDTO =
+                    GetUserAndRoles(UserName);
+
+                return View("EditRoles", objUserAndRolesDTO);
+            }
+        }
+        #endregion
+
         // Roles *****************************
 
         // GET: /Admin/ViewAllRoles
@@ -393,5 +538,217 @@ namespace WhiskeyJourney.Controllers
         }
         #endregion
 
+
+        // Utility
+
+        #region public ApplicationUserManager UserManager
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ??
+                       HttpContext.GetOwinContext()
+                           .GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        #endregion
+
+        #region public ApplicationRoleManager RoleManager
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ??
+                       HttpContext.GetOwinContext()
+                           .GetUserManager<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        #endregion
+
+        #region private List<SelectListItem> GetAllRolesAsSelectList()
+        private List<SelectListItem> GetAllRolesAsSelectList()
+        {
+            List<SelectListItem> SelectRoleListItems =
+                new List<SelectListItem>();
+
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(new ApplicationDbContext()));
+
+            var colRoleSelectList = roleManager.Roles.OrderBy(x => x.Name).ToList();
+
+            SelectRoleListItems.Add(
+                new SelectListItem
+                {
+                    Text = "Select",
+                    Value = "0"
+                });
+
+            foreach (var item in colRoleSelectList)
+            {
+                SelectRoleListItems.Add(
+                    new SelectListItem
+                    {
+                        Text = item.Name.ToString(),
+                        Value = item.Name.ToString()
+                    });
+            }
+
+            return SelectRoleListItems;
+        }
+        #endregion
+
+        #region private ExpandedUserDTO GetUser(string paramUserName)
+        private ExpandedUserDTO GetUser(string paramUserName)
+        {
+            ExpandedUserDTO objExpandedUserDTO = new ExpandedUserDTO();
+
+            var result = UserManager.FindByName(paramUserName);
+
+            // If we could not find the user, throw an exception
+            if (result == null) throw new Exception("Could not find the User");
+
+            objExpandedUserDTO.UserName = result.UserName;
+            objExpandedUserDTO.Email = result.Email;
+            objExpandedUserDTO.LockoutEndDateUtc = result.LockoutEndDateUtc;
+            objExpandedUserDTO.AccessFailedCount = result.AccessFailedCount;
+            objExpandedUserDTO.PhoneNumber = result.PhoneNumber;
+
+            return objExpandedUserDTO;
+        }
+        #endregion
+
+        #region private ExpandedUserDTO UpdateDTOUser(ExpandedUserDTO objExpandedUserDTO)
+        private ExpandedUserDTO UpdateDTOUser(ExpandedUserDTO paramExpandedUserDTO)
+        {
+            ApplicationUser result =
+                UserManager.FindByName(paramExpandedUserDTO.UserName);
+
+            // If we could not find the user, throw an exception
+            if (result == null)
+            {
+                throw new Exception("Could not find the User");
+            }
+
+            result.Email = paramExpandedUserDTO.Email;
+
+            // Lets check if the account needs to be unlocked
+            if (UserManager.IsLockedOut(result.Id))
+            {
+                // Unlock user
+                UserManager.ResetAccessFailedCountAsync(result.Id);
+            }
+
+            UserManager.Update(result);
+
+            // Was a password sent across?
+            if (!string.IsNullOrEmpty(paramExpandedUserDTO.Password))
+            {
+                // Remove current password
+                var removePassword = UserManager.RemovePassword(result.Id);
+                if (removePassword.Succeeded)
+                {
+                    // Add new password
+                    var AddPassword =
+                        UserManager.AddPassword(
+                            result.Id,
+                            paramExpandedUserDTO.Password
+                        );
+
+                    if (AddPassword.Errors.Count() > 0)
+                    {
+                        throw new Exception(AddPassword.Errors.FirstOrDefault());
+                    }
+                }
+            }
+
+            return paramExpandedUserDTO;
+        }
+        #endregion
+
+        #region private void DeleteUser(ExpandedUserDTO paramExpandedUserDTO)
+        private void DeleteUser(ExpandedUserDTO paramExpandedUserDTO)
+        {
+            ApplicationUser user =
+                UserManager.FindByName(paramExpandedUserDTO.UserName);
+
+            // If we could not find the user, throw an exception
+            if (user == null)
+            {
+                throw new Exception("Could not find the User");
+            }
+
+            UserManager.RemoveFromRoles(user.Id, UserManager.GetRoles(user.Id).ToArray());
+            UserManager.Update(user);
+            UserManager.Delete(user);
+        }
+        #endregion
+
+        #region private UserAndRolesDTO GetUserAndRoles(string UserName)
+        private UserAndRolesDTO GetUserAndRoles(string UserName)
+        {
+            // Go get the User
+            ApplicationUser user = UserManager.FindByName(UserName);
+
+            List<UserRoleDTO> colUserRoleDTO =
+            (from objRole in UserManager.GetRoles(user.Id)
+                select new UserRoleDTO
+                {
+                    RoleName = objRole,
+                    UserName = UserName
+                }).ToList();
+
+            if (colUserRoleDTO.Count() == 0)
+            {
+                colUserRoleDTO.Add(new UserRoleDTO { RoleName = "No Roles Found" });
+            }
+
+            ViewBag.AddRole = new SelectList(RolesUserIsNotIn(UserName));
+
+            // Create UserRolesAndPermissionsDTO
+            UserAndRolesDTO objUserAndRolesDTO =
+                new UserAndRolesDTO();
+            objUserAndRolesDTO.UserName = UserName;
+            objUserAndRolesDTO.colUserRoleDTO = colUserRoleDTO;
+            return objUserAndRolesDTO;
+        }
+        #endregion
+
+        #region private List<string> RolesUserIsNotIn(string UserName)
+        private List<string> RolesUserIsNotIn(string UserName)
+        {
+            // Get roles the user is not in
+            var colAllRoles = RoleManager.Roles.Select(x => x.Name).ToList();
+
+            // Go get the roles for an individual
+            ApplicationUser user = UserManager.FindByName(UserName);
+
+            // If we could not find the user, throw an exception
+            if (user == null)
+            {
+                throw new Exception("Could not find the User");
+            }
+
+            var colRolesForUser = UserManager.GetRoles(user.Id).ToList();
+            var colRolesUserInNotIn = (from objRole in colAllRoles
+                where !colRolesForUser.Contains(objRole)
+                select objRole).ToList();
+
+            if (colRolesUserInNotIn.Count() == 0)
+            {
+                colRolesUserInNotIn.Add("No Roles Found");
+            }
+
+            return colRolesUserInNotIn;
+        }
+        #endregion
     }
 }
