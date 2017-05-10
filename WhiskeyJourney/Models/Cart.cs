9c271d61012a9f7b1637d86655b4e6bd8c3ded1e
NTRandall <NathanTRandall@gmail.com>
2017-05-10 10:01:47 -0400

    added customer, car, product models and dbset

---
+++ WhiskeyJourney/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WhiskeyJourney.Models
+{
+    public class Cart
+    {
+        public int CartID { get; set; }
+        public int ProductID { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
---
+++ WhiskeyJourney/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhiskeyJourney.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+    }
+}
